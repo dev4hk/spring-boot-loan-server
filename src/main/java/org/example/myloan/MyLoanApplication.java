eea493e7f1c7695e5d4d0814f96fc8fe1e0b385f
Hoon <hoon0661@gmail.com>
2024-03-19 17:35:13 -0400

    initial commit

---
+++ src/main/java/org/example/myloan/MyLoanApplication.java
@@ -0,0 +1,13 @@
+package org.example.myloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLoanApplication.class, args);
+	}
+
+}
