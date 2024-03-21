b086b4edfb785a85aac9192d0bac069495f90d34
Hoon <hoon0661@gmail.com>
2024-03-21 08:06:21 -0400

    added implementation for creating application

--- src/main/java/org/example/myloan/service/ApplicationServiceImpl.java
+++ src/main/java/org/example/myloan/service/ApplicationServiceImpl.java
@@ -1,28 +1,35 @@
 package org.example.myloan.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.myloan.domain.Application;
 import org.example.myloan.dto.ApplicationDto;
+import org.example.myloan.dto.ApplicationDto.Request;
+import org.example.myloan.dto.ApplicationDto.Response;
 import org.example.myloan.repository.ApplicationRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class ApplicationServiceImpl implements ApplicationService{
     private final ApplicationRepository applicationRepository;
+    private final ModelMapper modelMapper;
 
 
     @Override
-    public ApplicationDto.Response create(ApplicationDto.Request request) {
+    public Response create(Request request) {
+        Application application = modelMapper.map(request, Application.class);
+        Application created = applicationRepository.save(application);
+        return modelMapper.map(created, Response.class);
+    }
+
+    @Override
+    public Response get(Long applicationId) {
         return null;
     }
 
     @Override
-    public ApplicationDto.Response get(Long applicationId) {
-        return null;
-    }
-
-    @Override
-    public ApplicationDto.Response update(Long applicationId, ApplicationDto.Request request) {
+    public Response update(Long applicationId, Request request) {
         return null;
     }
 
