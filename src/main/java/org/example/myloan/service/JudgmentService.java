2d50a0461eb1bae658ba085fbf046078e76432a9
Hoon <hoon0661@gmail.com>
2024-03-21 17:00:07 -0400

    added initial judgment service & repository

---
+++ src/main/java/org/example/myloan/service/JudgmentService.java
@@ -0,0 +1,9 @@
+package org.example.myloan.service;
+
+import org.example.myloan.dto.JudgmentDto.Request;
+import org.example.myloan.dto.JudgmentDto.Response;
+
+public interface JudgmentService {
+    Response create(Request requeset);
+
+}
---
+++ src/main/java/org/example/myloan/service/JudgmentServiceImpl.java
@@ -0,0 +1,22 @@
+package org.example.myloan.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.myloan.dto.JudgmentDto;
+import org.example.myloan.dto.JudgmentDto.Request;
+import org.example.myloan.dto.JudgmentDto.Response;
+import org.example.myloan.repository.JudgmentRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JudgmentServiceImpl implements JudgmentService{
+
+    private final JudgmentRepository judgmentRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public Response create(Request requeset) {
+        return null;
+    }
+}
