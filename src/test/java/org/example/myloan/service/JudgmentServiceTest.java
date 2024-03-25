cf38b2d51954711d584abd1edc351a46854b63f8
Hoon <hoon0661@gmail.com>
2024-03-25 17:29:19 -0400

    removed @DisplayName

--- src/test/java/org/example/myloan/service/JudgmentServiceTest.java
+++ src/test/java/org/example/myloan/service/JudgmentServiceTest.java
@@ -28,7 +28,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@DisplayName("")
 @ExtendWith(MockitoExtension.class)
 class JudgmentServiceTest {
 
