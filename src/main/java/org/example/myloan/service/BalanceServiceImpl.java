d4b1e3102a6b8bf8c7e6e2d7e733ad0c5abdd5a9
Hoon <hoon0661@gmail.com>
2024-03-25 10:19:51 -0400

    added service implementation for creating balance

--- src/main/java/org/example/myloan/service/BalanceServiceImpl.java
+++ src/main/java/org/example/myloan/service/BalanceServiceImpl.java
@@ -1,13 +1,18 @@
 package org.example.myloan.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.myloan.domain.Balance;
 import org.example.myloan.dto.BalanceDto;
 import org.example.myloan.dto.BalanceDto.Request;
 import org.example.myloan.dto.BalanceDto.Response;
 import org.example.myloan.dto.BalanceDto.UpdateRequest;
+import org.example.myloan.exception.BaseException;
+import org.example.myloan.exception.ResultType;
 import org.example.myloan.repository.BalanceRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @RequiredArgsConstructor
 @Service
@@ -18,7 +23,26 @@ public class BalanceServiceImpl implements BalanceService {
 
     @Override
     public Response create(Long applicationId, Request request) {
-        return null;
+        if(balanceRepository.findByApplicationId(applicationId).isPresent()) {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        }
+
+        Balance balance = modelMapper.map(request, Balance.class);
+
+        BigDecimal entryAmount = request.getEntryAmount();
+        balance.setApplicationId(applicationId);
+        balance.setBalance(entryAmount);
+
+        balanceRepository.findByApplicationId(applicationId).ifPresent(b -> {
+            balance.setBalanceId(b.getBalanceId());
+            balance.setIsDeleted(b.getIsDeleted());
+            balance.setCreatedAt(b.getCreatedAt());
+            balance.setUpdatedAt(b.getUpdatedAt());
+        });
+
+        balanceRepository.save(balance);
+
+        return modelMapper.map(balance, Response.class);
     }
 
     @Override
